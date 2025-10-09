191227251f1fa2a2be2ddf593064250af4a35f7d
POL-laway <paolocapelli02@icloud.com>
2025-10-09 21:12:20 +0200

    dipartimento-->corso-->lista insegnamenti  (manca entrare in ogni insegnamento ed integrare il codice già fatto)

--- ws_test.py
+++ ws_test.py
@@ -24,7 +24,7 @@ select = Select(select_element) # crea un oggetto Select
 selected_option = select.first_selected_option # ottieni l'opzione selezionata
 print(selected_option.text)  
 
-#entra nei dipartimenti
+#entra nei dipartimenti------------------------------------------------------
 dipartimenti_selector = '.u-font-text'
 dipartimenti = WebDriverWait(driver, 60).until(
     EC.presence_of_all_elements_located((By.CSS_SELECTOR, dipartimenti_selector)))
@@ -39,37 +39,95 @@ for i in range(len(dipartimenti)):
     EC.presence_of_element_located((By.CSS_SELECTOR, name_dipartimento_selector)))
    print(name_dipartimento.text)
 
-    # entra nei corsi
+    # entra nei corsi-----------------------------------------------------
    corsi_selector = '#main-content a'
    corsi = WebDriverWait(driver, 60).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, corsi_selector))
     )
 
-    # 1️⃣ estrai tutti gli href dei link trovati
+    #estrai tutti gli href dei link trovati
    links_corsi = []
    for corso in corsi:
         href = corso.get_attribute("href")
         if href:  # solo se l'attributo esiste
             links_corsi.append(href)
 
-    #2️⃣ accedi alle pagine web associate ai link
+    #accedi alle pagine web associate ai link
    for link in links_corsi:
         driver.get(link)  # naviga alla pagina del corso
 
-        # 3️⃣ estrai il nome del corso
+        #ESTRAZIONE DATI CORSO
+        #estrai il nome del corso
         name_corso_selector = '.u-filetto'
         name_corso = WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, name_corso_selector))
         )
-        print(name_corso.text)
 
-        # opzionale: torna indietro alla pagina dei corsi
+        #prendo il contenitore delle informazioni
+        contenitore = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".accordion"))
+        )
+        # prendo l'ultimo dd
+        last_dd = contenitore.find_element(By.XPATH, ".//dd[last()]")
+        # scrollo fino a quell'elemento
+        driver.execute_script("arguments[0].scrollIntoView({block:'center'});", last_dd)
+        last_dd.click()
+        # estraggo i dati dei primi e ultimi dd
+        last_dt  = contenitore.find_element(By.XPATH, ".//dt[last()]").text
+        first_dd = contenitore.find_element(By.XPATH, ".//dd[1]")
+        last_dd = contenitore.find_element(By.XPATH, ".//dd[last()]").text    
+
+        name_corso = name_corso.text.strip()  
+        id_corso = name_corso.split("]")[0].strip("[")     
+        name_corso = name_corso.split("]")[1]       
+        corso = {
+             "id": id_corso,
+             "titolo": name_corso
+
+         }
+              # Trova tutti i blocchi <dl> dentro il contenitore principale
+        blocchi = first_dd.find_elements(By.XPATH, ".//dl")
+
+        for blocco in blocchi:
+            # Etichetta (dt)
+            chiave = blocco.find_element(By.TAG_NAME, "dt").text.strip()
+            # Valore (dd)
+            valore = blocco.find_element(By.TAG_NAME, "dd").text.strip()
+            corso[chiave] = valore
+        
+        corso[last_dt] = last_dd
+
+        print(corso)
+
+        #entro nella pagina degli insegnamenti
+        btn_insegnamenti=WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, ".active~ li+ li a"))
+        )
+        btn_insegnamenti.click()
+        #prendo i link degli insegnamenti
+        insegnamenti_selector = '.flex-container a'
+        insegnamenti = WebDriverWait(driver, 60).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, corsi_selector))
+        )
+
+        #estrai tutti gli href dei link trovati
+        links_insegnamenti = []
+        for insegnamento in insegnamenti:
+            href = insegnamento.get_attribute("href")
+            if href:  # solo se l'attributo esiste
+                links_insegnamenti.append(href)
+
+        print(len(links_insegnamenti))
+        
+        #torna al blocco INFO
+        driver.back()
+        # torna indietro alla pagina dei corsi
         driver.back()
     #torna indietro ai dipartimenti
    driver.back()
 
 dipartimenti = WebDriverWait(driver, 60).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, dipartimenti_selector)))
-driver.back() #torna indietro ai dipartimenti
+driver.back()
     
 
 
